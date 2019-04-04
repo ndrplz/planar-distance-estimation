63cf7912dc2a5cac5c10603516ab83b573bf1057
ndrplz <ndrplz@gmail.com>
2019-04-04 17:21:57 +0200

    Add utils function for postprocessing detections

--- ssd_eval.py
+++ ssd_eval.py
@@ -1,12 +1,12 @@
+from typing import List
+
 import cv2
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
-from ssd.data import VOC_CLASSES as labels
-from ssd.ssd import build_ssd
-import matplotlib.pyplot as plt
 
-if torch.cuda.is_available():
-    torch.set_default_tensor_type('torch.cuda.FloatTensor')
+from ssd.data import VOC_CLASSES as voc_labels
+from ssd.ssd import build_ssd
 
 
 def ssd_preprocess(image: np.ndarray, resize_dim: int=300):
@@ -20,44 +20,95 @@ def ssd_preprocess(image: np.ndarray, resize_dim: int=300):
     return torch.from_numpy(x).permute(2, 0, 1)
 
 
-net = build_ssd('test', size=300, num_classes=21)  # initialize SSD
-net.load_weights('/media/minotauro/DATA/ai4automotive/ssd300_mAP_77.43_v2.pth')
-if torch.cuda.is_available():
-    net = net.cuda()
+def postprocess_detections(ssd_detections: torch.Tensor,
+                           min_confidence: float=0.0,
+                           dataset:str ='VOC'):
+    """
+    Post-process SSD detections, discarding those under confidence threshold.
+    
+    :param ssd_detections: torch.Tensor of shape (batch, n_classes, n_boxes, 5) 
+    :param min_confidence: Min confidence for the detection to be reliable
+    :param dataset: Dataset detected classes belong to 
+    :return: List of post-processed detections 
+    """
+    if dataset != 'VOC':
+        raise NotImplementedError('COCO support not implemented yet.')
 
-image = '/media/minotauro/DATA/ai4automotive/frames/001810.jpg'
-image = cv2.imread(image)
+    detections = ssd_detections.to('cpu').numpy()
+    _, n_classes, n_detections, _ = detections.shape
 
-x = ssd_preprocess(image, resize_dim=300)
+    output = []
 
-if torch.cuda.is_available():
-    x = x.cuda()
+    for i in range(n_classes):
 
-with torch.no_grad():
-    y = net(x.unsqueeze(0))
+        j = 0
 
-min_confidence = 0.1
+        # Detection proposals are already sorted by confidence score
+        while detections[0, i, j, 0] >= min_confidence:
 
-plt.figure(figsize=(10, 10))
-colors = plt.cm.hsv(np.linspace(0, 1, 21)).tolist()
-plt.imshow(image[:, :, ::-1])  # plot the image for matplotlib
-currentAxis = plt.gca()
+            list_item = {
+                'score': detections[0, i, j, 0],
+                'name': voc_labels[i - 1],
+                'coords': detections[0, i, j, 1:]
+            }
 
-detections = y.data
-# scale each detection back up to the image
-scale = torch.Tensor(image.shape[1::-1]).repeat(2)
-for i in range(detections.size(1)):
-    j = 0
-    while detections[0, i, j, 0] >= min_confidence:
-        score = detections[0, i, j, 0]
-        label_name = labels[i - 1]
-        display_txt = '%s: %.2f' % (label_name, score)
-        pt = (detections[0, i, j, 1:] * scale).cpu().numpy()
-        coords = (pt[0], pt[1]), pt[2] - pt[0] + 1, pt[3] - pt[1] + 1
-        color = colors[i]
-        currentAxis.add_patch(
-            plt.Rectangle(*coords, fill=False, edgecolor=color, linewidth=2))
-        currentAxis.text(pt[0], pt[1], display_txt,
-                         bbox={'facecolor': color, 'alpha': 0.5})
-        j += 1
-plt.show()
+            output.append(list_item)
+
+            j += 1
+
+    return output
+
+
+def draw_detections(frame: np.ndarray,
+                    postprocessed_detections: List[dict],
+                    color_palette: List[tuple],
+                    dataset: str='VOC',
+                    line_thickness: int=3):
+    if dataset != 'VOC':
+        raise NotImplementedError('COCO support not implemented yet.')
+
+    h, w, c = frame.shape
+    for d in postprocessed_detections:
+        idx = voc_labels.index(d['name'])
+        color = color_palette[idx]
+        color = [int(c * 255) for c in color]
+        coords = [w, h, w, h] * d['coords']
+        xmin, ymin, xmax, ymax = map(int, coords)
+        frame = cv2.rectangle(frame, (xmin, ymin), (xmax, ymax), color,
+                              thickness=line_thickness)
+    return frame
+
+
+if __name__ == '__main__':
+
+    if torch.cuda.is_available():
+        torch.set_default_tensor_type('torch.cuda.FloatTensor')
+
+    net = build_ssd('test', size=300, num_classes=21)  # initialize SSD
+    net.load_weights('/media/minotauro/DATA/ai4automotive/ssd300_mAP_77.43_v2.pth')
+    if torch.cuda.is_available():
+        net = net.cuda()
+
+    # todo net.eval()
+
+    image = '/media/minotauro/DATA/ai4automotive/frames/001810.jpg'
+    image = cv2.imread(image)
+
+    x = ssd_preprocess(image, resize_dim=300)
+
+    if torch.cuda.is_available():
+        x = x.cuda()
+
+    with torch.no_grad():
+        y = net(x.unsqueeze(0))
+
+    output = postprocess_detections(y, min_confidence=0.1)
+
+    palette_rgb = plt.cm.hsv(np.linspace(0, 1, len(voc_labels) + 1)).tolist()
+    palette_bgr = [p[:-1][::-1] for p in palette_rgb]
+    # todo palette in BGR
+    image_show = draw_detections(image, output, palette_bgr)
+
+    cv2.imshow('detected', image_show)
+    cv2.waitKey(0)
+
